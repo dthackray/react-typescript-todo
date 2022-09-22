7e9ba28efbcabbc69714b320fb2b928331856c41
Dan Thackray <djthackray@gmail.com>
2022-09-22 16:08:48 +0100

    Populate add task function
    Signed-off-by: Dan Thackray <djthackray@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ form?.addEventListener("submit", e => {
 
   if (input?.value == "" || input?.value == null) return
 
-  const newTask = {
+  const newTask: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
@@ -28,5 +28,12 @@ form?.addEventListener("submit", e => {
 })
 
 function addListItem(task: Task) {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
 
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
 }
