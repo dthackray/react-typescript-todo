e246bc5358177204d79b21f2269d14f00103f8fd
Dan Thackray <djthackray@gmail.com>
2022-09-22 15:57:37 +0100

    Add optional chaining to inputs
    Signed-off-by: Dan Thackray <djthackray@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,9 @@ const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.querySelector<HTMLFormElement>("#new-task-form")
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
 // const list = document.querySelector("#list")
+
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+
+  if (input?.value === "" || input?.value === null) return
+})
